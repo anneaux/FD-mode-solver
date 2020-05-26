a1e86f3217b7f3dc2739cada92f8bfdb9c9cc831
anneaux <a.weber@uni-jena.de>
2020-05-27 01:41:57 +0200

    Fixing solution of task 2

--- Homework_1_Group2.py
+++ Homework_1_Group2.py
@@ -35,20 +35,10 @@ def guided_modes_1DTE(prm, k0, h):
     L = (np.diag(diag_elements, 0) + np.diag(adj_elements, -1) + np.diag(adj_elements, 1))/ k0**2
 
     eff_eps, field = np.linalg.eig(L)
-    # f = plt.figure()
-    # plt.plot(x,prm-e_substrate, label = "permittivity")
-    # plt.plot(x, guided_modes[:,0,i], label = "field")
-    # plt.plot(eff_eps)
-    # print(e_substrate)
-    # print(max(prm))
-    # plt.show()
     for i in range(len(eff_eps)):
         if (eff_eps[i] <= e_substrate or eff_eps[i] >= max(prm)):
              eff_eps[i] = 0
              field[:,i] = 0
-    # g = plt.figure()
-    # plt.plot(eff_eps)
-    # plt.show()
     guided = field[:,np.where(field[1]!=0)]
     return eff_eps[np.where(eff_eps!=0)], guided
     pass
@@ -81,27 +71,28 @@ def guided_modes_2D(prm, k0, h, numb):
 
     # initializing the 2D mode operator
     L = sps.lil_matrix(sps.eye(N**2))
-    for j in range(0,N): #
-        for k in range(0,N):
-            index = k*N + j # mein Punkt
+    for j in range(0,N): # col of grid ( = x)
+        for k in range(0,N): # row of grid ( = y)
+            index = k*N + j # grid point number
             L[index,index] = -4/h**2 + k0**2 * prm_2D[k,j]
-            if (index +1 < N**2):
+            if (index + 1 < N**2 and (index+1)%N != 0): # right neighbor
                 L[index,index+1] = 1
-            if index -1 >= 0:
+            if (index - 1 >= 0 and index%N != 0): # left neighbor
                 L[index,index-1] = 1
-            if index +N < N**2:
+            if (index + N < N**2): # lower neighbor
                 L[index,index+N] = 1
-            if index -N >= 0:
+            if (index - N >= 0): # upper neighbor
                 L[index,index-N] = 1
 
     eigenvalues, eigenvectors = eigs(L, k = numb, which = "LR", maxiter = 1000)
-    return eigenvalues, eigenvectors
+    eff_eps = eigenvalues/k0**2
+    return eff_eps, eigenvectors
     pass
 
 
 # Test Values
-grid_size     = 50 #100 # please choose appropriate value = N
-number_points = 100 #200 # please choose appropriate value
+grid_size     = 100 #100 # please choose appropriate value = N
+number_points = 200 #200 # please choose appropriate value
 # h             = ?? # please choose appropriate value
 lam           = 0.78
 k0            = 2*np.pi/lam
@@ -109,19 +100,19 @@ e_substrate   = 1.5**2
 delta_e       = 1.5e-2
 w             = 15.0
 
-h = grid_size/ number_points
-x = np.linspace(-grid_size/2, grid_size/2, number_points)
+h = grid_size / number_points
+x = np.linspace(-grid_size/2, grid_size/2, number_points+1)
 y = x
 
-# Gaussian waveguide profile
+# Gaussian waveguide profiles
 prm_1D = e_substrate + delta_e * np.exp(- (x/w)**2)
 prm_2D = np.array([[e_substrate + delta_e * np.exp(- (ix**2 + iy**2)/w**2) for ix in x] for iy in y])
-
-# TASK 1
 # checking the gaussian permittivity profile and therefor suitable x values
 # plt.figure()
 # plt.plot(x,prm_1D)
 # plt.show()
+
+# TASK 1
 # eff_eps, guided_modes = guided_modes_1DTE(prm_1D, k0, h)
 # print(eff_eps)
 # print(guided_modes)
@@ -132,4 +123,4 @@ prm_2D = np.array([[e_substrate + delta_e * np.exp(- (ix**2 + iy**2)/w**2) for i
 #     plt.show()
 
 # TASK 2
-print(guided_modes_2D(prm_2D, k0, h, 10))
+print(guided_modes_2D(prm_2D, k0, h, 5))
