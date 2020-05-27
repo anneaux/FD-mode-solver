ed230c30bf2afdd3095c9f8aeedee9516585e3b8
anneaux <a.weber@uni-jena.de>
2020-05-27 21:58:12 +0200

    Tweaking solution of task 2

--- Homework_1_Group2.py
+++ Homework_1_Group2.py
@@ -76,13 +76,13 @@ def guided_modes_2D(prm, k0, h, numb):
             index = k*N + j # grid point number
             L[index,index] = -4/h**2 + k0**2 * prm_2D[k,j]
             if (index + 1 < N**2 and (index+1)%N != 0): # right neighbor
-                L[index,index+1] = 1
+                L[index,index+1] = 1/h**2
             if (index - 1 >= 0 and index%N != 0): # left neighbor
-                L[index,index-1] = 1
+                L[index,index-1] = 1/h**2
             if (index + N < N**2): # lower neighbor
-                L[index,index+N] = 1
+                L[index,index+N] = 1/h**2
             if (index - N >= 0): # upper neighbor
-                L[index,index-N] = 1
+                L[index,index-N] = 1/h**2
 
     eigenvalues, eigenvectors = eigs(L, k = numb, which = "LR", maxiter = 1000)
     eff_eps = eigenvalues/k0**2
@@ -91,8 +91,8 @@ def guided_modes_2D(prm, k0, h, numb):
 
 
 # Test Values
-grid_size     = 100 #100 # please choose appropriate value = N
-number_points = 200 #200 # please choose appropriate value
+grid_size     = 100 # please choose appropriate value = N
+number_points = 200 # please choose appropriate value
 # h             = ?? # please choose appropriate value
 lam           = 0.78
 k0            = 2*np.pi/lam
@@ -107,20 +107,40 @@ y = x
 # Gaussian waveguide profiles
 prm_1D = e_substrate + delta_e * np.exp(- (x/w)**2)
 prm_2D = np.array([[e_substrate + delta_e * np.exp(- (ix**2 + iy**2)/w**2) for ix in x] for iy in y])
-# checking the gaussian permittivity profile and therefor suitable x values
-# plt.figure()
+# # checking the gaussian permittivity profile and therefor suitable x values
+# f1 = plt.figure(dpi=100)
 # plt.plot(x,prm_1D)
+# plt.ylabel(r'Dielectric Permittivity $\epsilon(x)$')
+# plt.xlabel(r'x [$\mu$m]')
+# # plt.gca().invert_xaxis()
+# f1.tight_layout()
 # plt.show()
 
 # TASK 1
-# eff_eps, guided_modes = guided_modes_1DTE(prm_1D, k0, h)
-# print(eff_eps)
-# print(guided_modes)
+eff_eps, guided_modes = guided_modes_1DTE(prm_1D, k0, h)
 # for i in range(len(eff_eps)):
 #     f = plt.figure()
 #     plt.plot(x, prm_1D - e_substrate, label = "permittivity")
 #     plt.plot(x, guided_modes[:,0,i], label = "field")
 #     plt.show()
 
+# # Plot the intensities for the first six eigenmodes
+# f2, axs = plt.subplots(2,3,dpi=130,figsize=(10,5))
+# axs = axs.flatten()
+# for i in range(len(axs)):
+#     prm_dev = prm_1D - e_substrate # permittivity deviation from e_substrate
+#     intensity = np.abs(guided_modes[:,0,i])**2 / max(np.abs(guided_modes[:,0,i]))**2 * prm_dev # normalization
+#     axs[i].plot(x,prm_dev, label = r"$\epsilon$ deviation")
+#     axs[i].plot(x,intensity, label = "intensity [a.u.]")
+#     axs[i].set_xlim([-40,80])
+#     axs[i].set_ylim([-0.001,0.019])
+#     axs[i].set_ylabel("")
+#     axs[i].set_xlabel(r'position x [$\mu$m]')
+#     axs[i].legend(loc="upper right")
+#     title = 'Mode {}, '.format(i+1) + r'$\epsilon_{eff}$ = ' + '{:.3f}'.format(np.real(eff_eps[i]))
+#     axs[i].set_title(title)
+#     f2.tight_layout()
+# plt.show()
+
 # TASK 2
-print(guided_modes_2D(prm_2D, k0, h, 5))
+eff_eps_2D, fields_2D = guided_modes_2D(prm_2D, k0, h, 5)
